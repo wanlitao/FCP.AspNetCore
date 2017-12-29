d5f2309f8a180213fcbde4ca4c5aa279a05031f2
万里涛 <wanlitao_3055601@outlook.com>
2017-12-29 18:37:16 +0800

    添加项目文件。

---
+++ src/FCP.AspNetCore.Mvc.Formatters.Json/Extensions/MvcJsonOptionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public static class MvcJsonOptionsExtensions
+    {
+        public static MvcJsonOptions UseNullValueIgnore(this MvcJsonOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
+            return options;
+        }
+    }
+}
